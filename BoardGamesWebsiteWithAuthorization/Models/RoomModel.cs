8f17fe51d763488a0cbb8983f572cc658bdf8be3
Dawidz02 <dawidzaremba0331@gmail.com>
2024-11-11 17:24:05 +0100

    Podstawowe wyświetlanie pokoi

--- BoardGamesWebsiteWithAuthorization/Models/RoomModel.cs
+++ BoardGamesWebsiteWithAuthorization/Models/RoomModel.cs
@@ -8,14 +8,17 @@ namespace BoardGamesWebsiteWithAuthorization.Models
         public string RoomName { get; set; }
         public int IdGame { get; set; }
         public GameModel Game { get; set; }
-        public int IdHost { get; set; }
+        public string GameName { get; set; }
+        public string IdHost { get; set; }
+        public string HostName { get; set; }  // This is the new property
         public UserModel Host { get; set; }
         public bool Availability { get; set; }
         public string Localization { get; set; }
         public DateTime CreatingDate { get; set; }
         public string RoomDescription { get; set; }
-        public byte SkillLevelRequired { get; set; } // TINYINT CHECK (0-10)
+        public int SkillLevelRequired { get; set; } // TINYINT CHECK (0-10)
         public bool WhoCanJoin { get; set; }
+        public string MainGameImageUrl { get; set; } // Main image URL for the game
 
         public ICollection<RoomParticipantModel> RoomParticipants { get; set; }
         public ICollection<RoomInvitationModel> RoomInvitations { get; set; }
