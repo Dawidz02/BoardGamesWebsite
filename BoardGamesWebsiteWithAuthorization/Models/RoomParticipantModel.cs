24175bdc3830b768b356483847dfb7837e19964f
Dawidz02 <dawidzaremba0331@gmail.com>
2024-11-19 13:51:37 +0100

    Dodanie edycji pokoi

--- BoardGamesWebsiteWithAuthorization/Models/RoomParticipantModel.cs
+++ BoardGamesWebsiteWithAuthorization/Models/RoomParticipantModel.cs
@@ -1,14 +1,1 @@
-﻿using BoardGamesWebsiteWithAuthorization.Components.Pages;
-
-namespace BoardGamesWebsiteWithAuthorization.Models
-{
-    public class RoomParticipantModel
-    {
-        public int Id { get; set; }
-        public int IdRoom { get; set; }
-        public RoomModel Room { get; set; }
-        public int IdUser { get; set; }
-        public UserModel User { get; set; }
-        public DateTime JoiningDate { get; set; }
-    }
-}
+﻿                              c
