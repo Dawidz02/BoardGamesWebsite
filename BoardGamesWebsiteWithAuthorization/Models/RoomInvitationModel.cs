1eef21e61f8380fca35ecfc2c664f1e8dfae2824
Dawidz02 <dawidzaremba0331@gmail.com>
2024-11-21 16:22:25 +0100

    Obsługa zaproszeń do pokoju

--- BoardGamesWebsiteWithAuthorization/Models/RoomInvitationModel.cs
+++ BoardGamesWebsiteWithAuthorization/Models/RoomInvitationModel.cs
@@ -7,9 +7,9 @@ namespace BoardGamesWebsiteWithAuthorization.Models
         public int Id { get; set; }
         public int IdRoom { get; set; }
         public RoomModel Room { get; set; }
-        public int IdUser { get; set; }
+        public string IdUser { get; set; }
         public UserModel User { get; set; }
-        public int InvitedBy { get; set; }
+        public string InvitedBy { get; set; }
         public UserModel InvitedByUser { get; set; }
         public string Status { get; set; } = "Waiting"; // ENUM('Waiting', 'Accepted', 'Declined')
         public DateTime SendingDate { get; set; }
