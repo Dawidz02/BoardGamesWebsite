ff6508c5b4e68f6f30992ac71001b8a0b6f090f3
Dawidz02 <dawidzaremba0331@gmail.com>
2024-11-25 23:33:30 +0100

    Dodanie chatu w pokojach

---
+++ BoardGamesWebsiteWithAuthorization/Models/ChatMessageModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BoardGamesWebsiteWithAuthorization.Models
+{
+    public class ChatMessageModel
+    {
+        public int Id { get; set; }
+        public int IdRoom { get; set; }
+        public string IdUser { get; set; }
+        public string UserName { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime SentDate { get; set; }
+    }
+}
